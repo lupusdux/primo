bd104e44a52067fdef833d78492cfd6b1415f215
lupusdux <junio.songwriter@gmail.com>
2018-10-31 11:59:48 -0300

    3a edicao

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Sistema - Primo
                 </div>
 
                 <div class="links">
